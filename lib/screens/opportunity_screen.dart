556556b1d99bde10ea6585502cc7f61abbc4d20f
argyro <argyrogounari@gmail.com>
2020-04-18 13:43:12 +0100

    Add listview

--- lib/screens/opportunity_screen.dart
+++ lib/screens/opportunity_screen.dart
@@ -43,48 +43,51 @@ class _OpportunityScreenState extends State<OpportunityScreen> {
                   ),
                 ),
               ),
-              Padding(
+              IconButton(
                 padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
-                child: IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  iconSize: 30.0,
-                  color: Colors.black,
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
+                icon: Icon(Icons.arrow_back),
+                iconSize: 30.0,
+                color: Colors.black,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 220.0),
-                child: Container(
-                  padding: EdgeInsets.all(30.0),
-                  height: MediaQuery.of(context).size.height - 220.0,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    boxShadow: [
-                      new BoxShadow(
-                        color: Theme.of(context).primaryColor,
-                        offset: new Offset(-20.0, -10.0),
-                        blurRadius: 40.0
-                      )
-                    ],
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        widget.opportunity.title.toString().trim(),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.w600,
-                          color: Theme.of(context).primaryColor,
-                        ),
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: ListView(
+                  padding: EdgeInsets.only(top: 220.0),
+                  children: <Widget>[
+                    Container(
+
+                      height: MediaQuery.of(context).size.height - 220.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        boxShadow: [
+                          new BoxShadow(
+                            color: Theme.of(context).primaryColor,
+                            offset: new Offset(-20.0, -10.0),
+                            blurRadius: 40.0
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            widget.opportunity.title.toString().trim(),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
