5049d5d96f5dbdb0786d0e911bfc770f117c2eb3
argyro <argyrogounari@gmail.com>
2020-04-20 15:48:49 +0100

    Add basic working title search

--- lib/screens/opportunities_list.dart
+++ lib/screens/opportunities_list.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:erasmusopportunitiesapp/models/opportunity.dart';
 
 import 'opportunity_screen.dart';
 
-class OpportunitiesList extends StatefulWidget {
+class OpportunitiesList extends StatelessWidget {
 
   final List<Opportunity> opportunities;
+  OpportunitiesList({this.opportunities, UniqueKey key});
 
-  OpportunitiesList({this.opportunities});
-
-  @override
-  _OpportunitiesListState createState() => _OpportunitiesListState();
-}
-
-class _OpportunitiesListState extends State<OpportunitiesList> {
   @override
   Widget build(BuildContext context) {
-
     return ListView.builder(
       padding: EdgeInsets.only(top: 10.0, bottom: 15.0),
-      itemCount: widget.opportunities.length,
+      itemCount: opportunities.length,
       itemBuilder: (BuildContext context, int index) {
         return GestureDetector(
           onTap: () => Navigator.push(
             context,
             MaterialPageRoute(
                 builder: (_) => OpportunityScreen(
-                  opportunity: widget.opportunities[index],
+                  opportunity: opportunities[index],
                 )
             ),
           ),
@@ -59,7 +51,7 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
                               Container(
                                 width: 190.0,
                                 child: Text(
-                                  widget.opportunities[index].title.toString().trim(),
+                                  opportunities[index].title.toString().trim(),
                                   maxLines: 3,
                                   overflow: TextOverflow.ellipsis,
                                   style: TextStyle(
@@ -72,7 +64,7 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
                           ),
                           SizedBox(height: 5.0,),
                           Text(
-                            widget.opportunities[index].topic,
+                            opportunities[index].topic,
                             maxLines: 2,
                             overflow: TextOverflow.ellipsis,
                           ),
@@ -86,7 +78,7 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
                                     child: Icon(Icons.location_on, color: Colors.blueGrey, size: 11.7,),
                                   ),
                                 ),
-                                TextSpan(text: '' + widget.opportunities[index].venueLocation),
+                                TextSpan(text: '' + opportunities[index].venueLocation),
                               ],
                             ),
                           ),
@@ -101,7 +93,7 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
                               borderRadius: BorderRadius.circular(10.0),
                             ),
                             alignment: Alignment.center,
-                            child: Text(widget.opportunities[index].getPreviewType()),
+                            child: Text(opportunities[index].getPreviewType()),
                           ),
                           SizedBox(width: 10.0,),
                           Container(
@@ -111,7 +103,7 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
                               borderRadius: BorderRadius.circular(10.0),
                             ),
                             alignment: Alignment.center,
-                            child: Text(widget.opportunities[index].getPreviewParticipationCost()),
+                            child: Text(opportunities[index].getPreviewParticipationCost()),
                           ),
                           SizedBox(width: 10.0,),
                           Container(
@@ -121,7 +113,7 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
                               borderRadius: BorderRadius.circular(10.0),
                             ),
                             alignment: Alignment.center,
-                            child: Text(widget.opportunities[index].getPreviewDuration()),
+                            child: Text(opportunities[index].getPreviewDuration()),
                           ),
                         ],
                       ),
@@ -134,12 +126,12 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
                 top: 15.0,
                 bottom: 15.0,
                 child: Hero(
-                  tag: widget.opportunities[index].oid,
+                  tag: opportunities[index].oid,
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20.0),
                     child: Image(
                       width: 110.0,
-                      image: AssetImage(widget.opportunities[index].image),
+                      image: AssetImage(opportunities[index].image),
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -152,3 +144,152 @@ class _OpportunitiesListState extends State<OpportunitiesList> {
     );
   }
 }
+
+//
+//class OpportunitiesList extends StatefulWidget {
+//
+//  final List<Opportunity> opportunities;
+//  OpportunitiesList({this.opportunities, UniqueKey key});
+//
+//  @override
+//  _OpportunitiesListState createState() => _OpportunitiesListState();
+//}
+//
+//class _OpportunitiesListState extends State<OpportunitiesList> {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+//    return ListView.builder(
+//      padding: EdgeInsets.only(top: 10.0, bottom: 15.0),
+//      itemCount: widget.opportunities.length,
+//      itemBuilder: (BuildContext context, int index) {
+//        return GestureDetector(
+//          onTap: () => Navigator.push(
+//            context,
+//            MaterialPageRoute(
+//                builder: (_) => OpportunityScreen(
+//                  opportunity: widget.opportunities[index],
+//                )
+//            ),
+//          ),
+//          child: Stack(
+//            children: <Widget>[
+//              Container(
+//                margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+//                height: 180.0,
+//                width: double.infinity,
+//                decoration: BoxDecoration(
+//                  color: Colors.white,
+//                  borderRadius: BorderRadius.circular(20.0),
+//                ),
+//                child: Padding(
+//                  padding: EdgeInsets.fromLTRB(100.0, 15.0, 20.0, 20.0),
+//                  child: Column(
+//                    crossAxisAlignment: CrossAxisAlignment.start,
+//                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                    children: <Widget>[
+//                      Column(
+//                        crossAxisAlignment: CrossAxisAlignment.start,
+//                        mainAxisAlignment: MainAxisAlignment.start,
+//                        children: <Widget>[
+//                          Row(
+//                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                            crossAxisAlignment: CrossAxisAlignment.center,
+//                            children: <Widget>[
+//                              Container(
+//                                width: 190.0,
+//                                child: Text(
+//                                  widget.opportunities[index].title.toString().trim(),
+//                                  maxLines: 3,
+//                                  overflow: TextOverflow.ellipsis,
+//                                  style: TextStyle(
+//                                    fontSize: 18.0,
+//                                    fontWeight: FontWeight.w600,
+//                                  ),
+//                                ),
+//                              ),
+//                            ],
+//                          ),
+//                          SizedBox(height: 5.0,),
+//                          Text(
+//                            widget.opportunities[index].topic,
+//                            maxLines: 2,
+//                            overflow: TextOverflow.ellipsis,
+//                          ),
+//                          RichText(
+//                            text: TextSpan(
+//                              style: Theme.of(context).textTheme.body1,
+//                              children: [
+//                                WidgetSpan(
+//                                  child: Padding(
+//                                    padding: EdgeInsets.symmetric(vertical: 2.7),
+//                                    child: Icon(Icons.location_on, color: Colors.blueGrey, size: 11.7,),
+//                                  ),
+//                                ),
+//                                TextSpan(text: '' + widget.opportunities[index].venueLocation),
+//                              ],
+//                            ),
+//                          ),
+//                        ],
+//                      ),
+//                      Row(
+//                        children: <Widget>[
+//                          Container(
+//                            width: 30.0,
+//                            decoration: BoxDecoration(
+//                              color: Theme.of(context).accentColor,
+//                              borderRadius: BorderRadius.circular(10.0),
+//                            ),
+//                            alignment: Alignment.center,
+//                            child: Text(widget.opportunities[index].getPreviewType()),
+//                          ),
+//                          SizedBox(width: 10.0,),
+//                          Container(
+//                            width: 50.0,
+//                            decoration: BoxDecoration(
+//                              color: Theme.of(context).accentColor,
+//                              borderRadius: BorderRadius.circular(10.0),
+//                            ),
+//                            alignment: Alignment.center,
+//                            child: Text(widget.opportunities[index].getPreviewParticipationCost()),
+//                          ),
+//                          SizedBox(width: 10.0,),
+//                          Container(
+//                            width: 65.0,
+//                            decoration: BoxDecoration(
+//                              color: Theme.of(context).accentColor,
+//                              borderRadius: BorderRadius.circular(10.0),
+//                            ),
+//                            alignment: Alignment.center,
+//                            child: Text(widget.opportunities[index].getPreviewDuration()),
+//                          ),
+//                        ],
+//                      ),
+//                    ],
+//                  ),
+//                ),
+//              ),
+//              Positioned(
+//                left: 20.0,
+//                top: 15.0,
+//                bottom: 15.0,
+//                child: Hero(
+//                  tag: widget.opportunities[index].oid,
+//                  child: ClipRRect(
+//                    borderRadius: BorderRadius.circular(20.0),
+//                    child: Image(
+//                      width: 110.0,
+//                      image: AssetImage(widget.opportunities[index].image),
+//                      fit: BoxFit.cover,
+//                    ),
+//                  ),
+//                ),
+//              )
+//            ],
+//          ),
+//        );
+//      },
+//    );
+//  }
+//}
--- lib/screens/opportunity_list_filters.dart
+++ lib/screens/opportunity_list_filters.dart
@@ -4,48 +4,57 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'opportunities_list.dart';
 
-class OpportunityListFilters extends StatefulWidget {
-  @override
-  _OpportunityListFiltersState createState() => _OpportunityListFiltersState();
-}
-
-class _OpportunityListFiltersState extends State<OpportunityListFilters> {
+class OpportunityListFilters extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
 
     final opportunities = Provider.of<List<Opportunity>>(context);
+    var filteredOpportunities = opportunities;
 
-    return Scaffold(
-      appBar: AppBar(
-        brightness: Brightness.light,
-        elevation: 0.0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        title: TextField(
-          onChanged: (title) {
-            //_filterOpportunities(title, DatabaseService().opportunities);
-          },
-          style: TextStyle(color: Colors.black),
-          textInputAction: TextInputAction.search,
-          decoration: InputDecoration(
-            suffixIcon: IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                setState(() {
+    void _filterOpportunities(value) {
+        filteredOpportunities = opportunities
+            .where((opportunity) =>
+            opportunity.title.toLowerCase().contains(value.toLowerCase()))
+            .toList();
+    }
 
-                });
+    return StatefulBuilder(
+      builder: (context, StateSetter setState) =>
+
+        Scaffold(
+          appBar: AppBar(
+            brightness: Brightness.light,
+            elevation: 0.0,
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            title: TextField(
+              onChanged: (title) {
+
+                setState(() => _filterOpportunities(title));
               },
+              style: TextStyle(color: Colors.black),
+              textInputAction: TextInputAction.search,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    setState(() {
+
+                    });
+                  },
+                ),
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+                hintText: 'Search titles',
+              ),
             ),
-            focusedBorder: InputBorder.none,
-            enabledBorder: InputBorder.none,
-            errorBorder: InputBorder.none,
-            disabledBorder: InputBorder.none,
-            contentPadding: EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
-            hintText: 'Search titles',
           ),
+          body: OpportunitiesList(opportunities: filteredOpportunities),
         ),
-      ),
-      body: OpportunitiesList(opportunities: opportunities),
+
     );
   }
 }
