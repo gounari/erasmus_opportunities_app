259e98402a42ca64c703104c37ada71c6cf72323
argyro <argyrogounari@gmail.com>
2020-04-30 00:05:35 +0100

    Add firebase storage

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,10 @@
 
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:erasmusopportunitiesapp/helpers/opportunity_constants.dart';
 import 'package:erasmusopportunitiesapp/models/opportunity.dart';
+import 'package:flutter/material.dart';
 
 class DatabaseService {
 
@@ -9,34 +12,37 @@ class DatabaseService {
   final CollectionReference opportunitiesCollection = Firestore.instance.collection(oppConstants.collection);
 
   List<Opportunity> _opportunityListFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.documents.map((doc){
+    List opportunities =  snapshot.documents.map((doc) {
       return Opportunity(
-        oid: doc.documentID,
-        title: doc[oppConstants.title],
-        organisationName: doc[oppConstants.organisationName],
-        organisationUID: doc[oppConstants.organisationUID],
-        venueAddress: doc[oppConstants.venueAddress],
-        venueCountry: doc[oppConstants.venueCountry],
-        participatingCountries: doc[oppConstants.participatingCountries],
-        type: doc[oppConstants.type],
-        startDate: doc[oppConstants.startDate].toDate(),
-        endDate: doc[oppConstants.endDate].toDate(),
-        lowAge: doc[oppConstants.lowAge],
-        highAge: doc[oppConstants.highAge],
-        topics: doc[oppConstants.topics],
-        applicationDeadline: doc[oppConstants.applicationDeadline].toDate(),
-        participationCost: doc[oppConstants.participationCost].toDouble(),
-        reimbursementLimit: doc[oppConstants.reimbursementLimit].toDouble(),
-        applicationLink: doc[oppConstants.applicationLink],
-        provideForDisabilities: doc[oppConstants.provideForDisabilities],
-        description: doc[oppConstants.description],
-        uploadTime: doc[oppConstants.uploadTime]
+      oid: doc.documentID,
+      title: doc[oppConstants.title],
+      organisationName: doc[oppConstants.organisationName],
+      organisationUID: doc[oppConstants.organisationUID],
+      venueAddress: doc[oppConstants.venueAddress],
+      venueCountry: doc[oppConstants.venueCountry],
+      participatingCountries: doc[oppConstants.participatingCountries],
+      type: doc[oppConstants.type],
+      startDate: doc[oppConstants.startDate].toDate(),
+      endDate: doc[oppConstants.endDate].toDate(),
+      lowAge: doc[oppConstants.lowAge],
+      highAge: doc[oppConstants.highAge],
+      topics: doc[oppConstants.topics],
+      applicationDeadline: doc[oppConstants.applicationDeadline].toDate(),
+      participationCost: doc[oppConstants.participationCost].toDouble(),
+      reimbursementLimit: doc[oppConstants.reimbursementLimit].toDouble(),
+      applicationLink: doc[oppConstants.applicationLink],
+      provideForDisabilities: doc[oppConstants.provideForDisabilities],
+      description: doc[oppConstants.description],
+      uploadTime: doc[oppConstants.uploadTime]
       );
     }).toList();
+
+    return opportunities;
   }
 
   Stream<List<Opportunity>> get opportunities {
     return opportunitiesCollection.orderBy(oppConstants.startDate).snapshots()
         .map(_opportunityListFromSnapshot);
   }
+
 }
