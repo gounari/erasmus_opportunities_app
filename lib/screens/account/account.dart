f6d4b9c306511e7430ab5a313065bb602b839b13
argyro <argyrogounari@gmail.com>
2020-05-02 17:27:49 +0100

    Add onboarding

--- lib/screens/account/account.dart
+++ lib/screens/account/account.dart
@@ -1,4 +1,6 @@
+import 'package:erasmusopportunitiesapp/services/auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Account extends StatefulWidget {
   @override
@@ -6,10 +8,22 @@ class Account extends StatefulWidget {
 }
 
 class _AccountState extends State<Account> {
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Account'),
+      child: FlatButton.icon(
+        onPressed: () async {
+          await _auth.signOut();
+        },
+        icon: Icon(
+          Icons.exit_to_app,
+          color: Color.fromRGBO(0, 68, 148, 1),
+        ),
+        label: Text(''),
+      ),
     );
   }
 }
--- lib/screens/account/onboarding.dart
+++ lib/screens/account/onboarding.dart
@@ -1,15 +1,65 @@
+
+
+import 'package:erasmusopportunitiesapp/onboarding/flutter_login.dart';
+import 'package:erasmusopportunitiesapp/onboarding/models/login_data.dart';
+import 'package:erasmusopportunitiesapp/onboarding/models/signup_data.dart';
+import 'package:erasmusopportunitiesapp/screens/account/account.dart';
+import 'package:erasmusopportunitiesapp/services/auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class OnBoarding extends StatefulWidget {
-  @override
-  _OnBoardingState createState() => _OnBoardingState();
-}
+class OnBoarding extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
 
-class _OnBoardingState extends State<OnBoarding> {
+  final AuthService _auth = AuthService();
+
+  Future<String> _signInUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) async {
+
+      dynamic user = await _auth.signInWithEmailAndPassword(data.email, data.password);
+      if (user == null) {
+        return 'Error signing in';
+      }
+
+      return null;
+    });
+  }
+
+  Future<String> _signUpUser(SignUpData data) async {
+    return Future.delayed(loginTime).then((_) async {
+
+      dynamic user = await _auth.registerWithEmailAndPassword(data.email, data.password, data.organisationName, data.organisationLocation);
+      if (user == null) {
+        return 'Error signing up';
+      }
+
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+
+      //TODO
+
+      return null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("OnBoarding"),
+    return FlutterLogin(
+      title: '',
+      logo: 'assets/images/logo_white.png',
+      onLogin: _signInUser,
+      onSignup: _signUpUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => Account(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
     );
   }
 }
